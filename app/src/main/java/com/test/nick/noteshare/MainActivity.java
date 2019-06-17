47fc3092fad3a7e74ec89ec34132af4237c44eab
Nick Silva <silva.d.nicholas@gmail.com>
2019-06-17 16:18:24 -0500

    added sticky note file, starting work on main page ui

--- app/src/main/java/com/test/nick/noteshare/MainActivity.java
+++ app/src/main/java/com/test/nick/noteshare/MainActivity.java
@@ -2,12 +2,16 @@ package com.test.nick.noteshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: starting program");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
