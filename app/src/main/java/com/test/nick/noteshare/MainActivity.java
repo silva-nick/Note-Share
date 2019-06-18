3eb15a108441324a0aa1739c211ffcb43b85fe38
Nick Silva <silva.d.nicholas@gmail.com>
2019-06-18 12:03:13 -0500

    Changes main layout, adds recycler view, begins add button functionality

--- app/src/main/java/com/test/nick/noteshare/MainActivity.java
+++ app/src/main/java/com/test/nick/noteshare/MainActivity.java
@@ -3,6 +3,7 @@ package com.test.nick.noteshare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starting program");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void addNote(View view){
 
     }
 }
