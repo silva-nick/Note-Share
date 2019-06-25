58731a644385dc9d1d499499f76a66c243fe93c7
Nick Silva <silva.d.nicholas@gmail.com>
2019-06-25 12:19:25 -0500

    changes ui slightly, changes transitions

--- app/src/main/java/com/test/nick/noteshare/StickyEditActivity.java
+++ app/src/main/java/com/test/nick/noteshare/StickyEditActivity.java
@@ -6,16 +6,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.transition.ChangeBounds;
+import android.transition.Explode;
+import android.transition.Transition;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
 public class StickyEditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setEnterTransition(new Explode());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sticky_edit);
-
         Button backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener(){
             @Override
