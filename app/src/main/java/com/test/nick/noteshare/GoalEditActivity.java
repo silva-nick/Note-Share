d57f428890ced0297caf10c49ea42a11647d07a1
Nick Silva <silva.d.nicholas@gmail.com>
2019-07-29 18:16:32 -0500

    starts work on the goal

---
+++ app/src/main/java/com/test/nick/noteshare/GoalEditActivity.java
@@ -0,0 +1,14 @@
+package com.test.nick.noteshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goal_edit);
+    }
+}
